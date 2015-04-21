9a836bfc77fa86e33f8609e5949bbc940d0cfa20
Ben Snape <ben.snape@itv.com>
2015-04-21 16:28:56 +0100

    trying to clone a repo

--- flow.groovy
+++ flow.groovy
@@ -1,3 +1,7 @@
+def clone(repo) {
+    git url: "http://github.com:bsnape/${repo}", branch: 'master'
+}
+
 def unit() {
     stage 'Unit Test'
     node('master') {
@@ -5,13 +9,19 @@ def unit() {
     }
 }
 
-//common = load 'scripts/common.groovy'
-
-//common.deploy()
-//unit()
-
-def runFlow() {
-    echo 'runFlow'
+def deploy() {
+    stage 'Deploy'
+    input message: "Are you sure you want to deploy?"
+    node('master') {
+        echo 'deploy step...'
+    }
 }
+
+repo = 'example-sensu-rspec-tests'
+
+
+clone(repo)
+deploy()
+unit()
 
 return this;
