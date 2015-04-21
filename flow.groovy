a6313d828b8b774675cc93776cf3c449950c7eb8
Ben Snape <ben.snape@itv.com>
2015-04-21 16:32:17 +0100

    fixed git url and moved node definition

--- flow.groovy
+++ flow.groovy
@@ -1,27 +1,25 @@
 def clone(repo) {
-    git url: "http://github.com:bsnape/${repo}", branch: 'master'
+    git url: "http://github.com/bsnape/${repo}.git", branch: 'master'
 }
 
 def unit() {
     stage 'Unit Test'
-    node('master') {
-        echo 'unit tests...'
-    }
+    echo 'unit tests...'
 }
 
 def deploy() {
     stage 'Deploy'
     input message: "Are you sure you want to deploy?"
-    node('master') {
-        echo 'deploy step...'
-    }
+    echo 'deploy step...'
 }
 
 repo = 'example-sensu-rspec-tests'
 
 
-clone(repo)
-deploy()
-unit()
+node {
+    clone(repo)
+    deploy()
+    unit()
+}
 
 return this;
